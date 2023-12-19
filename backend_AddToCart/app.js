ace36fde83358c3626287d54a718d8191490a32e
MalikHamzaFarooq <hamza.farooq@algorepublic.com>
2023-12-19 12:33:06 +0500

    Upload image module Added

--- backend_AddToCart/app.js
+++ backend_AddToCart/app.js
@@ -24,6 +24,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/uploads/:filename', (req, res) => {
